48430c51570942e4218a2134f8f885a7ceb65d6a
DonKane <109813504+Don24Kane@users.noreply.github.com>
2024-12-05 22:55:46 +0200

    Create random_key.sh

---
+++ useful_codies_nstuff/gitBASH/random_key.sh
@@ -0,0 +1,11 @@
+# by Don24Kane
+
+#!/usr/bin/env bash
+
+generateKey() {
+    K1='cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3'
+    K2='cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3'
+    K3='cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3'
+    K4='cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3'
+    PSK="$K1$K2$K3$K4"
+}
