a6f08d1fc209186c03b00e8791361baba994badd
DonKane <109813504+Don24Kane@users.noreply.github.com>
2024-12-05 22:48:12 +0200

    Create check_memory.sh

---
+++ useful_codies_nstuff/gitBASH/check_memory.sh
@@ -0,0 +1,10 @@
+# by Don24Kane
+
+#!/usr/bin/env bash
+
+phymem="$(free | awk '/^Mem:/{print $2}')"
+[ -z "$phymem" ] && phymem=0
+if [ "$phymem" -lt 1000000 ]; then
+  echoerr "A minimum of 1024 MB RAM is required."
+  exit 1
+fi
